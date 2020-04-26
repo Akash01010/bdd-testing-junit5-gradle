7c270ab8b0c0fb177436a164e480b1f0bd118c49
Akash Agrawal <akashagrawal01010@gmail.com>
2020-04-26 16:29:38 +0530

    Clear code

--- src/test/java/com/sapient/demo/DemoApplicationTest.java
+++ src/test/java/com/sapient/demo/DemoApplicationTest.java
@@ -1,26 +1,28 @@
 package com.sapient.demo;
 
-import com.github.mkolisnyk.cucumber.reporting.CucumberResultsOverview;
-import io.cucumber.java.After;
+//import com.github.mkolisnyk.cucumber.reporting.CucumberResultsOverview;
+//import io.cucumber.java.After;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DemoApplicationTest {
 
     @Test
     void main() {
+        Assertions.assertEquals(1,1);
     }
 
-    @After
-    void generateCucumberReports() {
-        CucumberResultsOverview results = new CucumberResultsOverview();
-        results.setOutputDirectory("target");
-        results.setOutputName("cucumber-results");
-        results.setSourceFile("./target/cucumber/cucumber.json");
-        try {
-            results.execute();
-        } catch (Exception e) {
-            System.out.println("Exception Thrown");
-        }
-    }
+//    @After
+//    void generateCucumberReports() {
+//        CucumberResultsOverview results = new CucumberResultsOverview();
+//        results.setOutputDirectory("target");
+//        results.setOutputName("cucumber-results");
+//        results.setSourceFile("./target/cucumber/cucumber.json");
+//        try {
+//            results.execute();
+//        } catch (Exception e) {
+//            System.out.println("Exception Thrown");
+//        }
+//    }
 
 }
